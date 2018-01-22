a1d2807b85b0a3b57c9b567537a4c005875430d4
ngesa marvin <marvinngesa@gmail.com>
2018-01-22 05:28:36 +0300

    call setupRGB() method

--- app/src/main/java/com/droidmarvin/peripheraliointerfacingrgbledwithapa102led_driver/MainActivity.java
+++ app/src/main/java/com/droidmarvin/peripheraliointerfacingrgbledwithapa102led_driver/MainActivity.java
@@ -22,6 +22,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setupRGB();
     }
 
     @Override
