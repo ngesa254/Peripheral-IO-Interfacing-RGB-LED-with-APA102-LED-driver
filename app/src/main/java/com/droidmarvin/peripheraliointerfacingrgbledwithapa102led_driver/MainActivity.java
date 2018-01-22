bcc313e7763e1dd887198d274ead3c1662866c39
ngesa marvin <marvinngesa@gmail.com>
2018-01-22 05:13:11 +0300

    things initial commit

---
+++ app/src/main/java/com/droidmarvin/peripheraliointerfacingrgbledwithapa102led_driver/MainActivity.java
@@ -0,0 +1,12 @@
+package com.droidmarvin.peripheraliointerfacingrgbledwithapa102led_driver;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
